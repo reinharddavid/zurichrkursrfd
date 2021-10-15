9dac0b7e4cf817ccc6bfeb6830d53ad027c6ef5e
reinharddavid <r.david@lignotrend.de>
2021-10-15 16:42:12 +0200

    Erstkommit

---
+++ R/schoenesSkript.R
@@ -0,0 +1,14 @@
+#' Irgendeinen Satz über R schreiben
+#'
+#' @param x A number
+#' @return Er schreibt einfach einen recht sinnlosen Satz.
+#' @examples
+#' firstfunction(1, 1)
+#' add(10, 1)
+#'
+#' @export
+
+firstfunction <- function(x) {
+  # print("Ein Königreich für ein R-Package!")
+  x
+}
